820b7a721d2a4e278b49882411f2451175d76e73
aayush09-dev <s24cseu2418@bennett.edu.in>
2026-03-08 13:39:53 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./Component/mainPage/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex">
+      <Sidebar />
+
+      <div className="flex-1 p-6">
+        <Routes>
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/users" element={<h1>Users</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
