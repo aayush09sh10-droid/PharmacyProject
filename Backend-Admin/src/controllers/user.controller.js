da76298935445dfc1742173cd143a3eb1befffb3
aayush09-dev <s24cseu2418@bennett.edu.in>
2026-03-24 11:05:33 +0530

    Login user

--- Backend-Admin/src/controllers/user.controller.js
+++ Backend-Admin/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model";
 import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler.util";
+import router from "../routes/user.routes";
+
 
 const registerUser= asyncHandler(async(req,res)=>{
     const {name,email,password,latitude,longitude,role}=req.body
@@ -25,3 +27,29 @@ const registerUser= asyncHandler(async(req,res)=>{
 
     })
 })
+const loginUser=asyncHandler(async(req,res)=>{
+    //get data 
+    //password match hash
+    //login name exist?
+    const {name,password,email}=req.body
+    if (!name || !email){
+        throw new ApiError(400,"user not exist")
+    }
+    const user= await User.findOne({
+        $or:[{name},{email}]
+
+    })
+    if(!user){
+        throw new ApiError(400,"User not found")
+
+    }
+    const isPasswordValid=await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(400,"Password incorrect")
+    }
+
+    
+})
+
+
+export {registerUser}
