e1e90cad1247028ae167ce809a3fc29be0377761
aayush09-dev <aayush09sh10@gmail.com>
2026-04-22 12:22:55 +0530

    Working

--- PP@/backend/src/routes/vendor.routes.js
+++ PP@/backend/src/routes/vendor.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { loginVendor } from '../controllers/vendor.controller.js';
+import { 
+  loginVendor, 
+  registerVendor
+   
+} from '../controllers/vendor.controller.js';
+
 const router = Router();
 
 router.route("/register").post(registerVendor);
 router.route("/login").post(loginVendor);
-router.route("/request").post(giveRequestOfVerification)
+// router.route("/request").post(giveRequestOfVerification);
 
 export default router;
